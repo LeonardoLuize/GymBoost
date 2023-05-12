9da72952e8aba5ded32a7d0f2024ea6b20925102
Leonardo Luize <leonardo.luize2@gmail.com>
2023-05-12 12:11:54 -0300

    feat: add exercise header

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,9 +1,38 @@
-import { Center, Text } from "native-base";
+import { Heading, HStack, Icon, Text, VStack } from "native-base";
+import { TouchableOpacity } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import { AppNavigatorRoutesProps } from "../routes/app.routes";
+import BodySvg from "../assets/body.svg";
 
-export function Exercise(){
-    return(
-        <Center flex={1}>
-            <Text color="white">Exercise</Text>
-        </Center>
-    )
+export function Exercise() {
+  const navigation = useNavigation<AppNavigatorRoutesProps>();
+
+  return (
+    <VStack flex={1}>
+      <VStack px={8} bg="gray.600" pt={12}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Icon as={Feather} name="arrow-left" color="green.500" size={6} />
+        </TouchableOpacity>
+
+        <HStack
+          justifyContent="space-between"
+          alignItems="center"
+          mt={8}
+          mb={6}
+        >
+          <Heading flexShrink={1} color="gray.100" fontSize="lg">
+            Supino
+          </Heading>
+
+          <HStack alignItems="center">
+            <BodySvg />
+            <Text color="gray.200" ml={1} textTransform="capitalize">
+              Biceps
+            </Text>
+          </HStack>
+        </HStack>
+      </VStack>
+    </VStack>
+  );
 }
